f7647230b89c536fb6448f584b03f5dbcffe52d5
Erika <117277361+cherryrainOuO@users.noreply.github.com>
2023-07-31 00:01:40 +0900

    G5_12865 ? DP
    https://www.acmicpc.net/problem/12865

---
+++ C#/DP/_12865.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BOJ
+{
+    internal class Boj
+    {
+        public static void Main()
+        {
+            var nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var arr = new List<Tuple<int, int>>();
+            arr.Add(new Tuple<int, int>(0, 0));
+
+            for (int i = 1; i <= nk[0]; i++)
+            {
+                int[] str;
+                using (var sr = new StringReader(Console.ReadLine()))
+                    str = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+
+                arr.Add(new Tuple<int, int>(str[0], str[1]));
+            }
+
+            arr = arr.OrderBy(n => n.Item1).ToList();
+
+            var dp = new int[nk[0] + 1, nk[1] + 1];
+
+            for (int i = 1; i <= nk[0]; i++)
+            {
+                for (int j = 1; j <= nk[1]; j++)
+                {
+                    if (arr[i].Item1 <= j)
+                    {
+                        dp[i, j] = Math.Max(arr[i].Item2 + dp[i - 1, j - arr[i].Item1],
+                                            dp[i - 1, j]);
+
+                    }
+                    else
+                    {
+                        dp[i, j] = dp[i - 1, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(dp[nk[0], nk[1]]);
+        }
+    }
+}
