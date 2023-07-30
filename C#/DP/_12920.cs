2c9514548c547353693bf250bdcd63882b93920a
Erika <117277361+cherryrainOuO@users.noreply.github.com>
2023-07-31 00:08:57 +0900

    P4_12920 ? DP
    https://www.acmicpc.net/problem/12920

---
+++ C#/DP/_12920.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BOJ
+{
+    internal class Boj
+    {
+        public static void Main()
+        {
+            var nm = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            var item = new List<Tuple<int, int>>();
+            item.Add(new Tuple<int, int>(0, 0));
+
+            for (int i = 1; i <= nm[0]; i++)
+            {
+                var arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                for (int j = 0; arr[2] > 0; j++)
+                {
+                    var pow = (int)Math.Min(Math.Pow(2, j), arr[2]);
+
+                    item.Add(new Tuple<int, int>(arr[0] * pow, arr[1] * pow));
+
+                    arr[2] -= pow;
+                }
+            }
+
+
+            var dp = new int[item.Count + 1, nm[1] + 1];
+
+            for (int i = 1; i <= item.Count; i++)
+            {
+                for (int j = 1; j <= nm[1]; j++)
+                {
+                    if (j - item[i - 1].Item1 < 0)
+                        dp[i, j] = dp[i - 1, j];
+                    else
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i - 1, j - item[i - 1].Item1] + item[i - 1].Item2);
+
+                }
+            }
+
+            Console.WriteLine(dp[item.Count, nm[1]]);
+
+        }
+    }
+}
