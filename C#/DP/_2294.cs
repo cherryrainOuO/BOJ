6627fa7dcd94c1e9eb73a0ae97a45c5264bca5c7
Erika <117277361+cherryrainOuO@users.noreply.github.com>
2023-08-01 01:28:40 +0900

    G5_2294 ? DP
    https://www.acmicpc.net/problem/2294

---
+++ C#/DP/_2294.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BOJ
+{
+    internal class Boj
+    {
+        public static void Main()
+        {
+            var nk = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            var item = new int[nk[0] + 1];
+                
+            for(int i=1; i <= nk[0]; i++)
+            {
+                item[i] = int.Parse(Console.ReadLine());
+            }
+
+            var dp = new int[nk[0] + 1, nk[1] + 1];
+
+            for (int i = 1; i <= nk[1]; i++) dp[0, i] = 1000001; 
+
+            for(int i=1;i <= nk[0]; i++)
+            {
+                for(int j=1; j <= nk[1]; j++)
+                {
+                    if (item[i] > j)
+                    {
+                        dp[i, j] = dp[i - 1, j];
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - item[i]] + 1);
+                    }
+                }
+            }
+
+            Console.WriteLine(dp[nk[0], nk[1]] == 1000001 ? -1 : dp[nk[0], nk[1]]);
+
+        }
+    }
+}
