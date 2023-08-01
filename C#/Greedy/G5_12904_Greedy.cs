8f5c1d26e6818af87b0eb87e618fe7d0d9ab5267
Erika <117277361+cherryrainOuO@users.noreply.github.com>
2023-08-01 22:38:18 +0900

    G5_12904 ? Greedy
    https://www.acmicpc.net/problem/12904
    
    이런 문제만 풀고 싶다

---
+++ C#/Greedy/G5_12904_Greedy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace BOJ
+{
+    internal class Boj
+    {
+        public static void Main()
+        {
+            var s = Console.ReadLine();
+            var t = Console.ReadLine();
+
+            for(int i=0; ; i++)
+            {
+                //Console.WriteLine(s + " " + t);
+                if (t.Length == s.Length) break;
+
+                if (t[t.Length - 1] == 'A')
+                {
+                    string temp = "";
+
+                    for (int j = 0; j < t.Length - 1; j++)
+                    {
+                        temp += t[j];
+                    }
+
+                    t = temp;
+                }
+                else if(t[t.Length - 1] == 'B')
+                {
+                    string temp = "";
+
+                    for (int j = 0; j < t.Length - 1; j++)
+                    {
+                        temp += t[j];
+                    }
+
+                    var k = temp.ToCharArray().Reverse().ToArray();
+
+                    t = new string(k);
+                }
+            }
+
+            Console.WriteLine(s == t ? 1 : 0);
+        }
+
+    }
+}
